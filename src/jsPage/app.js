702dc955094184b7346e119d72298d1d44d23529
神風 夜空 <kamikazelight@gmail.com>
2023-06-23 07:10:13 +0900

    最初のJavaScriptを追加

---
+++ src/jsPage/app.js
@@ -0,0 +1,66 @@
+﻿// 回答ボタン
+let $bts = document.getElementsByTagName('button')
+
+// 問題セット
+let qIndex = 0
+const quizList = [
+    {
+        question: 'jsでHTMLドキュメントを表すオブジェクトはどれ?',
+        answer: [
+            'MyDocuments', 
+            'MyDocument', 
+            'Documents', 
+            'Document'
+        ],
+        correct: 'Document'
+    },
+    {
+        question: 'jsの慣習でHTMLオブジェクトを入れる変数の先頭に付けられている記号はどれ?',
+        answer: [
+            '%', 
+            '$', 
+            '_',
+            '#'
+        ],
+        correct: '$'
+    }
+]
+
+
+const setBtEvent = () => {
+    btIndex = -1;
+    while(++btIndex < $bts.length){
+        $bts[btIndex].addEventListener('click', (e) => {
+            console.log(e)
+            if(quizList[qIndex].correct === e.target.textContent){
+                window.alert('正解!')
+                qIndex++;
+                if(qIndex < quizList.length){
+                    // なにもしない。
+                } else {
+                    window.alert('一問目に戻ります。')
+                    qIndex = 0
+                }
+                setupQuize(qIndex)
+            } else {
+                window.alert('不正解...')
+            }
+        });
+    }
+}
+
+const setupQuize = (qIndex) => {
+
+    // 問題文をセット
+    document.getElementById('js-question').textContent = quizList[qIndex].question
+
+    // 選択肢をセット
+    let ct2 = -1;
+    while(++ct2 < quizList[qIndex].answer.length){
+        $bts[ct2].textContent = quizList[qIndex].answer[ct2]
+    };
+}
+
+// 最初の問題をセット
+setupQuize(qIndex)
+setBtEvent()
---
+++ src/jsPage/index.js
@@ -0,0 +1,52 @@
+﻿console.clear()
+
+const langs = ['en', 'ja'];
+
+let langNum = 0;
+let lang = langs[langNum]
+
+const Messages = {
+  en: {
+    Hello: 'Hello!',
+    DeclarationOfCounting: 'We will now count.',
+    Counter: ['one', 'two', 'tree'],
+    Messages: [
+      'Show Tags',
+      'Hello World!',
+      'A simple primary alert—check it out!'
+    ]
+  },
+  ja: {
+    Hello: 'こんにちは',
+    DeclarationOfCounting: 'これから数を数えます。',
+    Counter: ['いち', 'に', 'さん'],
+    Messages: [
+      'タグを表示します。',
+      'こんにちは、世界！',
+      'シンプルな一次警報です！'
+    ]
+  }
+}
+
+console.log(Messages[lang].Hello);
+
+const ShowCounter = (arg) => {
+    console.log(Messages[lang].Counter[arg]);
+};
+
+console.log(Messages[lang].DeclarationOfCounting)
+
+let index = 0;
+while(index <= Messages[lang].Counter.length){
+  ShowCounter(index);
+  index++;
+}
+
+// console.log(Messages[lang].Messages[0])
+// document.getElementsByTagName('button')[0].addEventListener('click', ()=> {
+//   window.alert(Messages[lang].Messages[1])
+// });
+
+// document.getElementsByTagName('button')[1].addEventListener('click', ()=> {
+//   lang = langs[langNum++%langs.length]
+// });
