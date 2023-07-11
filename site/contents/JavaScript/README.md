f3759c8cc8a72fbdcddc5ee4a6d2da25f8c5117a
神風 夜空 <kamikazelight@gmail.com>
2023-07-12 06:57:50 +0900

    サイト構成変更

---
+++ site/contents/JavaScript/README.md
@@ -0,0 +1,6 @@
+# JavaScript
+
+JavaScript全般の内容や、その他の学習をここへ記す。
+
+
+[Demo](src/index.html)
---
+++ site/index.js
@@ -0,0 +1,24 @@
+﻿const menuList = [
+  {
+    Title: 'JavaScript',
+    href: 'contents//JavaScript//README.md'
+  },
+  {
+    Title: 'PowerShell(未実装)',
+    href: '#'
+  }
+]
+
+
+
+
+
+
+let index = 0;
+while(index <= menuList.length){
+  let $element = document.createElement('li')
+  $element.innerHTML = '<a href='+ menuList[index].href +'>'+ menuList[index].Title +'</a>'
+  let $menu = document.getElementById('menu')
+  $menu.appendChild($element)
+  index++;
+}
